9d1cd3d43a8c8860ec3e470b269fc5b9ade2f1a3
0xquantum3labs <contact@quantum3labs.com>
2024-04-30 23:24:31 +0800

    fix contract deploy vendor

--- packages/snfoundry/contracts/src/lib.cairo
+++ packages/snfoundry/contracts/src/lib.cairo
@@ -1,1 +1,2 @@
-mod HelloStarknet;
+mod Challenge2;
+mod YourToken;
--- packages/snfoundry/scripts_js/deploy.js
+++ packages/snfoundry/scripts_js/deploy.js
@@ -1,6 +1,22 @@
 const { deployer, deployContract } = require("./deploy_contract");
 const deployScript = async () => {
-  await deployContract(null, "HelloStarknet");
+  const your_token = await deployContract(
+    {
+      name: "Gold",
+      symbol: "GLD",
+      fixed_supply: 200_000_000_000_000_000_000n,
+      recipient:
+        "0x64b48806902a367c8598f4f95c305e8c1a1acba5f082d294a43793113115691",
+    },
+    "YourToken"
+    );
+  
+  const ch2 = await deployContract({
+    token_address: your_token.address,
+    eth_contract_address: "0x49D36570D4E46F48E99674BD3FCC84644DDD6B96F7C741B1562B82F9E004DC7",
+    owner: "0x64b48806902a367c8598f4f95c305e8c1a1acba5f082d294a43793113115691"
+  }
+    , "Challenge2");
 };
 
 deployScript()
